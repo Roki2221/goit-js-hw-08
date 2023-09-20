de4bc21bbdfdf114e20402a3b2dd293de9683ad8
Roki2221 <100676027+Roki2221@users.noreply.github.com>
2023-09-20 10:01:07 +0300

    8 start fix

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector('.gallery');
+galleryList.insertAdjacentHTML('beforeend', markup(galleryItems));
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+function markup(arr) {
+  return arr
+    .map(
+      ({ preview, description, original }) =>
+        `<li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+              <img class="gallery__image" src="${preview}" alt="${description}" />
+          </a>
+        </li>`
+    )
+    .join('');
+}
