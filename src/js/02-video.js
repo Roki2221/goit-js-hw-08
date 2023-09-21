b1745815d79cfaed90ad7819ca426957f6f103ce
Roki2221 <100676027+Roki2221@users.noreply.github.com>
2023-09-21 09:36:30 +0300

    second task done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
+const player = new Player('vimeo-player');
+const TIME_KEY = 'videoplayer-current-time';
+const timeValue = localStorage.getItem(TIME_KEY);
 
+player.setCurrentTime(JSON.parse(timeValue));
+
+player.on(
+  'timeupdate',
+  throttle(data => {
+    const time = data.seconds;
+    localStorage.setItem(TIME_KEY, JSON.stringify(time));
+  }, 1000)
+);
